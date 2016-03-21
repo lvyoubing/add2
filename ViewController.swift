d9706ced3056b8379a48191d732cf5ccf48cb3a6
lvyoubing <lvhuanda@admindeiMac-88.local>
2016-03-21 20:18:03 +0800

    加减乘除

--- ViewController.swift
+++ ViewController.swift
@@ -27,11 +27,55 @@ class ViewController: UIViewController {
         c=a+b
         z.text="\(c)"
     }
+    @IBAction func jian(sender: UIButton) {
+        var d:Double!=0//初始化变量值为0
+        var e:Double!=0//b初始化变量值为0
+        var f:Double!=0//c初始化变量值为0
+        
+        if(!x.text!.isEmpty){
+            d=(x.text!as NSString).doubleValue//类型转换为double
+        }
+        if(!y.text!.isEmpty){
+            e=(y.text!as NSString).doubleValue
+        }
+        f=d-e
+        z.text="\(f)"
+        
+        
+    }
+    @IBAction func cheng(sender: UIButton) {
+        var g:Double!=0//初始化变量值为0
+        var h:Double!=0//b初始化变量值为0
+        var i:Double!=0//c初始化变量值为0
+        
+        if(!x.text!.isEmpty){
+            g=(x.text!as NSString).doubleValue//类型转换为double
+        }
+        if(!y.text!.isEmpty){
+            h=(y.text!as NSString).doubleValue
+        }
+        i=g*h
+        z.text="\(i)"
+        
+    }
+    @IBAction func chu(sender: UIButton) {
+        var j:Double!=0//初始化变量值为0
+        var k:Double!=0//b初始化变量值为0
+        var l:Double!=0//c初始化变量值为0
+        
+        if(!x.text!.isEmpty){
+            j=(x.text!as NSString).doubleValue//类型转换为double
+        }
+        if(!y.text!.isEmpty){
+            k=(y.text!as NSString).doubleValue
+        }
+        l=j*k
+        z.text="\(l)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
